46812cc39de874ce4299684b84af69087f2ebf33
Rinkit Adhana <therinkit@gmail.com>
2025-06-30 07:33:03 +0530

    wrapper added

---
+++ src/layout/FileStructureWrapper.tsx
@@ -0,0 +1,9 @@
+const FileStructureWrapper = ({children}: {children: React.ReactNode}) => {
+  return (
+    <div className="md:max-w-[1216px] w-full mx-auto">
+      {children}
+    </div>
+  )
+}
+
+export default FileStructureWrapper
